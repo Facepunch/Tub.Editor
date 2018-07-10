49048eacb57c89e8ad513bcc0971990baf63015a
Garry Newman <garrynewman@users.noreply.github.com>
2018-07-10 13:35:37 +0100

    Export attributes

--- Assets/Internal/Exported/Tub.Entity/BaseEntity.cs
+++ Assets/Internal/Exported/Tub.Entity/BaseEntity.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 
 namespace Tub
 {
+	[TriggersEvent( "die", "Entity has died. A good place to spawn a ragdoll, or gibs" )]
+	[TriggersEvent( "die.gib", "Entity has died. The damage was enough to give it. If this isn't implemented we'll call die instead." )]
 	public class BaseEntity : Networked, INetworkObserved
 	{
 	   public EntityClass Classification;
--- Assets/Internal/Exported/Tub/DeathInfo.cs
+++ Assets/Internal/Exported/Tub/DeathInfo.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 
 namespace Tub
 {
+	[System.Serializable]
 	public struct DeathInfo
 	{
 	   public Tub.DeathType DeathType;
