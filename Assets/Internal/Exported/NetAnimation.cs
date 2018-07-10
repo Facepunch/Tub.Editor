0ed9bc08bd70c326048235383f123e78b23ddf77
Garry Newman <garrynewman@users.noreply.github.com>
2018-07-10 13:25:40 +0100

    Export Stub Functions

--- Assets/Internal/Exported/NetAnimation.cs
+++ Assets/Internal/Exported/NetAnimation.cs
@@ -8,4 +8,25 @@ public class NetAnimation : Networked, INetworkStatic
    public AnimationClip AnimationClip;
    public float Speed;
    public float Delta;
+
+   //
+   // Methods here stubbed so you can hook them up with the Unity event stuff
+   //
+
+   public void GetComponents()
+   {
+   }
+
+   public void UpdateToTime( float time,bool triggerevents )
+   {
+   }
+
+   public virtual void TriggerEvents( float mintime,float maxtime )
+   {
+   }
+
+   public virtual void SetClip( string name )
+   {
+   }
+
 }
--- Assets/Internal/Exported/NetAnimationControlled.cs
+++ Assets/Internal/Exported/NetAnimationControlled.cs
@@ -7,4 +7,29 @@ public class NetAnimationControlled : NetAnimation, INetworkObserved, INetworkSt
 {
    public float Target;
    public float Current;
+
+   //
+   // Methods here stubbed so you can hook them up with the Unity event stuff
+   //
+
+   public void SetValue( float delta )
+   {
+   }
+
+   public void Toggle()
+   {
+   }
+
+   public void SetOn()
+   {
+   }
+
+   public void SetOff()
+   {
+   }
+
+   public override void SetClip( string name )
+   {
+   }
+
 }
