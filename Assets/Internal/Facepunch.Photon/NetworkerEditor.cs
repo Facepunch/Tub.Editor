4f1053c4cce18d1b7505c43e3ce1585d3cc9e3de
Garry Newman <garrynewman@users.noreply.github.com>
2018-07-11 11:47:36 +0100

    Automatically set up networker settings

--- Assets/Internal/Facepunch.Photon/NetworkerEditor.cs
+++ Assets/Internal/Facepunch.Photon/NetworkerEditor.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine.SceneManagement;
 
 namespace Facepunch
 {
@@ -15,14 +17,14 @@ namespace Facepunch
             if ( t == null )
                 return;
 
-            //    EditorGUILayout.BeginVertical( GUI.skin.box );
-
             if ( Application.isPlaying )
             {
                 EditorGUILayout.LabelField( $"View ID: {t.viewIdField}", EditorStyles.whiteMiniLabel );
                 EditorGUILayout.LabelField( $"This Owner ID: {t.viewIdField}", EditorStyles.whiteMiniLabel );
                 return;
             }
+
+            ApplyNetworkerConstraints( t );
 
             EditorGUIUtility.labelWidth = 100;
 
@@ -71,6 +73,76 @@ namespace Facepunch
             return c.GetType().Name;
         }
 
+        static string NetworkertHash( Networker networker )
+        {
+            return $"{string.Join( ";", networker.ObservedComponents.Select( x => x?.ToString() ) )}-{networker.ownershipTransfer}-{networker.synchronization}-{networker.SyncTransforms}";
+        }
+
+        public static void ApplyNetworkerConstraints( Networker networker )
+        {
+            if ( networker.ObservedComponents == null )
+            {
+                networker.ObservedComponents = new List<Component>();
+            }
+
+            var hash = NetworkertHash( networker );
+
+            var observables = networker.GetComponents<INetworkObservable>();
+
+            networker.ObservedComponents.Clear();
+            networker.ObservedComponents.AddRange( observables.Select( x => x as Component ) );
+
+            if ( networker.SyncTransforms && networker.GetComponent<INetworkStatic>() != null )
+            {
+                networker.SyncTransforms = false;
+            }
+
+            if ( !networker.SyncTransforms )
+            {
+                networker.ObservedComponents.Remove( networker );
+            }
+
+            if ( networker.GetComponent<INetworkTakeover>() != null )
+            {
+                networker.ownershipTransfer = OwnershipOption.Takeover;
+            }
+
+            if ( networker.GetComponent<INetworkTakeover>() == null )
+            {
+                networker.ownershipTransfer = OwnershipOption.Fixed;
+            }
+
+            if ( networker.ObservedComponents.Count > 0 )
+            {
+                networker.synchronization = ViewSynchronization.UnreliableOnChange;
+            }
+
+            if ( networker.ObservedComponents.Count == 0 )
+            {
+                networker.synchronization = ViewSynchronization.Off;
+            }
+
+            if ( hash != NetworkertHash( networker ) )
+            {
+               // Debug.Log( $"Updating Networker {networker.gameObject.name} - {hash}" );
+                UnityEditor.EditorUtility.SetDirty( networker.gameObject );
+            }
+
+        }
+
+        [PostProcessScene]
+        public static void ApplyNetworkerConstraintsAll()
+        {
+            Facepunch.Networker[] views = SceneManager.GetActiveScene().GetRootGameObjects()
+                                    .SelectMany( x => x.GetComponentsInChildren<Facepunch.Networker>( true ) )
+                                    .OrderBy( x => x.gameObject.GetInstanceID() )
+                                    .ToArray();
+
+            foreach ( var view in views )
+            {
+                ApplyNetworkerConstraints( view );
+            }
+        }
     }
 }
 
