83329a07414dba5a43c4eab5ef5b15c8a4eb935a
Garry Newman <garrynewman@users.noreply.github.com>
2018-07-16 16:16:08 +0100

    World Tip

---
+++ Assets/Internal/Exported/WorldTip.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using Facepunch;
+using System.Collections.Generic;
+
+
+namespace Tub
+{
+	public class WorldTip : MonoBehaviour
+	{
+	   public string Name;
+	   public string Description;
+	   public Transform Origin;
+	}
+}
