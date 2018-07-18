1feeb6dc49c8ef44eb55b7023c8b6e7327b892c6
Garry Newman <garrynewman@users.noreply.github.com>
2018-07-18 13:46:46 +0100

    RagdollStickTrigger

---
+++ Assets/Internal/Exported/Tub.Game/RagdollStickTrigger.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using Facepunch;
+using System.Collections.Generic;
+
+
+namespace Tub
+{
+	public class RagdollStickTrigger : MonoBehaviour
+	{
+	   public Rigidbody AttachTo;
+	}
+}
