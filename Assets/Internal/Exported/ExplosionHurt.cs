c7a83cdccffc70ca7a0964753e3a83adef19253e
Garry Newman <garrynewman@users.noreply.github.com>
2018-07-31 13:52:11 +0100

    Added explosions to Demo Hurt

---
+++ Assets/Internal/Exported/ExplosionHurt.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using Facepunch;
+using System.Collections.Generic;
+
+
+namespace Tub
+{
+	public class ExplosionHurt : MonoBehaviour
+	{
+	   public float Radius;
+	   public float Damage;
+
+	   //
+	   // Methods here stubbed so you can hook them up with the Unity event stuff
+	   //
+
+	   public void DoDamage()
+	   {
+	   }
+
+	}
+}
---
+++ Assets/Internal/Exported/SpawnClientsideObject.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using Facepunch;
+using System.Collections.Generic;
+
+
+namespace Tub
+{
+	public class SpawnClientsideObject : MonoBehaviour
+	{
+	   public GameObject ObjectToSpawn;
+	   public Transform Parent;
+
+	   //
+	   // Methods here stubbed so you can hook them up with the Unity event stuff
+	   //
+
+	   public void DoSpawn()
+	   {
+	   }
+
+	}
+}
